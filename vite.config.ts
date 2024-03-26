207f129050f9fd5d6d96ea709ceeeb69319a1860
Pofi Abel <abel@barnksfortetech.com>
2024-03-26 11:31:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dotenv from 'dotenv';
 
 dotenv.config(); 
+
+const mode = process.env.NODE_ENV || "development";
+process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
 // https://vitejs.dev/config/
 export default defineConfig({
