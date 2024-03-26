99a84a4e6f6b094a31eafc92f3e8874f7c2ca0a4
Pofi Abel <abel@barnksfortetech.com>
2024-03-26 12:32:30 +0100

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import dotenv from 'dotenv';
-
-dotenv.config(); 
-
-const mode = process.env.NODE_ENV || "development";
-process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+import { defineConfig, loadEnv } from "vite";
+import http from "https";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.SERVER_URI,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.SERVER_URI,
+          changeOrigin: true,
+          agent: new http.Agent(),
+        },
       },
     },
-  },
+  };
 });
